5a52c61e913f152bf0d2b5ce7091e1256d10c5da
Michael Tong <michaelyuyangtong@gmail.com>
2022-07-09 19:16:36 +0800

    Added splitbee AB testing

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Layout from "../components/Layout";
 import PrivateRoute from "../components/PrivateRoute";
 import PublicRoute from "../components/PublicRoute";
 import { AuthProvider } from "../contexts/AuthContext";
+import Script from "next/script";
 import "../styles/globals.css";
 
 const noAuthPages = [
@@ -19,22 +20,25 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <AuthProvider>
-      {noAuthPages.includes(router.pathname) ? (
-        <PublicRoute>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </PublicRoute>
-      ) : (
-        <PrivateRoute>
-          <Layout>
-            <Component {...pageProps} />
-            <Toaster />
-          </Layout>
-        </PrivateRoute>
-      )}
-    </AuthProvider>
+    <>
+      <Script async src="https://cdn.splitbee.io/sb.js" />
+      <AuthProvider>
+        {noAuthPages.includes(router.pathname) ? (
+          <PublicRoute>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </PublicRoute>
+        ) : (
+          <PrivateRoute>
+            <Layout>
+              <Component {...pageProps} />
+              <Toaster />
+            </Layout>
+          </PrivateRoute>
+        )}
+      </AuthProvider>
+    </>
   );
 }
 
